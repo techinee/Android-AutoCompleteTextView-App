ebc01018c74c727cbba38791622d4c4ac2e62521
techinee <6206021621066@fitm.kmutnb.ac.th>
2022-02-10 15:16:01 +0700

    Finish

---
+++ app/src/main/java/com/techinee/autocompletetextviewapp/MainActivity.java
@@ -0,0 +1,48 @@
+package com.techinee.autocompletetextviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageButton;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private AutoCompleteTextView acTextView;
+    private MultiAutoCompleteTextView macTextView;
+    private String [] inputs = {"Pakistan","Iran","China","India","Thailand","U.A.E","Yemen",
+                                "Palestine","America","Australia","Turkey","Endland","France","Greece"};
+    private ImageButton btnSave;
+    private int pos = 0;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        acTextView = (AutoCompleteTextView) findViewById(R.id.acTextView);
+        macTextView = (MultiAutoCompleteTextView) findViewById(R.id.macTextView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,inputs);
+        acTextView.setAdapter(adapter);
+        macTextView.setAdapter(adapter);
+        macTextView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+        btnSave = (ImageButton) findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(this);
+    }
+
+
+    @Override
+    public  void onClick(View v) {
+        String message = "";
+        message = "acTextView = " +acTextView.getText().toString();
+        message += "\nmacTextView = " + macTextView.getText().toString();
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+    }
+}
